cb4b92accfadbe830891d42d7730ff792ca85f5c
Fabiana <f.catania@ismea.it>
2024-08-05 15:58:13 +0200

    aggiuta registrazione tramite laravel breeze

--- routes/web.php
+++ routes/web.php
@@ -1,45 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('benvenuto');
-});
-
-/*
-Route::get('/', function () {
     return view('welcome');
 });
-*/
-Route::get('users/', function () {
-    return view('profile');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('Pratiche/', function () {
-    return view('PratichePresentate');
-});
-
-// routes/web.php
-
-use App\Http\Controllers\PracticeController;
-
-Route::resource('practices', PracticeController::class);
-
-
-
-// Rotta per visualizzare il modulo di caricamento
-use App\Http\Controllers\FileUploadController;
-
-Route::get('upload', function () {
-    return view('upload');
-});
-
-// Rotta per gestire il caricamento del file
-Route::post('upload', [FileUploadController::class, 'store'])->name('upload');
-
-
-
-
-
-
-
+require __DIR__.'/auth.php';
